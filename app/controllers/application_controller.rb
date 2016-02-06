292d9690ae7340dca0a2f93513787e69c4d61a88
Lluís Gili <lluis@ingent.net>
2016-02-06 17:13:48 +0100

    catch routing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def error404
+    render text: 'not found', status: 404
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
+  # catch all routing errors
+  match '*path', via: :all, to: 'application#error404'
 end
