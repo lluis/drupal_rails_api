e13e5050200618c17e0bcb290bde44808f7863b6
Lluís Gili <lluis@ingent.net>
2016-02-23 18:39:52 +0100

    hide non published events

--- db/migrate/20160205163718_create_events_view.rb
+++ db/migrate/20160205163718_create_events_view.rb
@@ -6,12 +6,18 @@ class CreateEventsView < ActiveRecord::Migration[5.0]
         SELECT node.nid AS id,
                node.title AS title,
                field_data_body.body_value AS description,
-               field_data_field_data.field_data_value AS date
+               field_data_field_data.field_data_value AS date,
+               file_managed.uri as image
         FROM "node"
         JOIN field_data_body ON field_data_body.entity_id=node.nid
         JOIN field_data_field_data ON field_data_field_data.entity_id=node.nid
-        WHERE type='activitat'
+        JOIN file_usage ON file_usage.id=node.nid
+             AND file_usage.type='node'
+             AND file_usage.module='file'
+        JOIN file_managed ON file_managed.fid=file_usage.fid
+        WHERE node.type='activitat'
           AND field_data_field_data.field_data_rrule is null
+          AND node.status=1
     SQL
   end
 
